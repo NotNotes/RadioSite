c2670936b4ee4debb8ab9d78c162277d426f0ba2
w0zzah <70504456+w0zzah@users.noreply.github.com>
2020-09-04 11:46:57 +1200

    e

---
+++ site/script.js
@@ -0,0 +1,54 @@
+// Original source code provided by Anna Monus
+// https://codepen.io/tutsplus/pen/bGVPVrY
+
+/* Toggle mobile menu */
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+toggle.addEventListener("click", toggleMenu, false);
+
+
+
+/* Activate Submenu */
+const items = document.querySelectorAll(".item");
+
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+
+
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+
+document.addEventListener("click", closeSubmenu, false);
